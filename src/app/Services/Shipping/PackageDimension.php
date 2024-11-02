a0be679847d95cfc149584a0515e4b31301f7667
alex_nemynov <alexnemynov@yandex.ru>
2024-11-02 21:53:20 +0300

    3.24 VO

---
+++ src/app/Services/Shipping/PackageDimension.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Shipping;
+
+class PackageDimension
+{
+    public function __construct(public readonly int $height, public readonly int $width, public readonly int $length)
+    {
+        match (true) {
+            $this->width <= 0 || $this->width > 80 => throw new \InvalidArgumentException('Invalid package width'),
+            $this->height <= 0 || $this->height > 70 => throw new \InvalidArgumentException('Invalid package height'),
+            $this->length <= 0 || $this->length > 120 => throw new \InvalidArgumentException('Invalid package length'),
+            default => true,
+        };
+    }
+
+    public function increaseWidth(int $width): self
+    {
+        return new self($this->height, $this->width + $width, $this->length);
+    }
+
+    public function equalTo(PackageDimension $other): bool
+    {
+        return $this->height === $other->height && $this->width === $other->width && $this->length === $other->length;
+    }
+}
